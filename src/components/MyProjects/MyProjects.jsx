79d014cc0c6c7e96c7ec8b986701d61bba3df7df
bibek190 <bibek9818917036@gmail.com>
2024-02-27 23:42:24 +1100

    project section

--- src/components/MyProjects/MyProjects.jsx
+++ src/components/MyProjects/MyProjects.jsx
@@ -1,36 +1,68 @@
 import React from "react";
 import "./MyProjects.css";
+import { Link, NavLink } from "react-router-dom";
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 const MyProjects = () => {
   return (
     <section className="myProjects">
       <h2>My Projects</h2>
       <div className="project-container">
-        <div className="individual-project">
-          <img
-            src="https://imageio.forbes.com/blogs-images/forbestechcouncil/files/2019/01/canva-photo-editor-8-7.jpg?height=640&width=640&fit=bounds"
-            alt=""
-          />
+        <div className="project-box ">
+          <img src="./images/project1.jpeg" alt="" />
           <div className="project-content">
-            <h3>My 1st project</h3>
+            <h4>Project1</h4>
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
-              quam maxime obcaecati quis magnam a consequatur, provident ex
-              eveniet dolor!
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A omnis
+              voluptate beatae dolorem commodi. Repellat ut voluptatibus magnam
+              at harum.
             </p>
+            <NavLink to="#">
+              <FaExternalLinkAlt />
+            </NavLink>
           </div>
         </div>
-        <div className="individual-project">
-          <img
-            src="https://imageio.forbes.com/blogs-images/forbestechcouncil/files/2019/01/canva-photo-editor-8-7.jpg?height=640&width=640&fit=bounds"
-            alt=""
-          />
+        <div className="project-box">
+          <img src="./images/project2.jpeg" alt="" />
+          <div className="project-content">
+            <h4>Project1</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A omnis
+              voluptate beatae dolorem commodi. Repellat ut voluptatibus magnam
+              at harum.
+            </p>
+            <NavLink to="#">
+              <FaExternalLinkAlt />
+            </NavLink>
+          </div>
         </div>
-        <div className="individual-project">
-          <img
-            src="https://imageio.forbes.com/blogs-images/forbestechcouncil/files/2019/01/canva-photo-editor-8-7.jpg?height=640&width=640&fit=bounds"
-            alt=""
-          />
+        <div className="project-box">
+          <img src="./images/project3.jpeg" alt="" />
+          <div className="project-content">
+            <h4>Project1</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A omnis
+              voluptate beatae dolorem commodi. Repellat ut voluptatibus magnam
+              at harum.
+            </p>
+            <NavLink to="#">
+              <FaExternalLinkAlt />
+            </NavLink>
+          </div>
+        </div>
+        <div className="project-box">
+          <img src="./images/project4.jpeg" alt="" />
+          <div className="project-content">
+            <h4>Project1</h4>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. A omnis
+              voluptate beatae dolorem commodi. Repellat ut voluptatibus magnam
+              at harum.
+            </p>
+            <NavLink to="#">
+              <FaExternalLinkAlt />
+            </NavLink>
+          </div>
         </div>
       </div>
     </section>
